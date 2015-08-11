6bbe4555b4509c5dabfa273c12d3d4ff47977ba7
Vic van Gool <vic@cloud66.com>
2015-08-11 18:10:04 +0100

    trying to inline

--- lita_config.rb
+++ lita_config.rb
@@ -1,9 +1,9 @@
-Dir["plugins/*.rb"].each {|file| require file }
+Dir['handlers/*.rb'].each { |file| require_relative file }
 
 Lita.configure do |config|
-  config.robot.log_level = ENV['LITA_INFO_LEVEL'].to_sym
-  config.redis[:host] = ENV['REDIS_HOST']
-  config.redis[:port] = ENV['REDIS_PORT']
-  config.robot.adapter = :slack
-  config.adapters.slack.token = ENV['SLACK_TOKEN']
+	config.robot.log_level = ENV['LITA_INFO_LEVEL'].to_sym
+	config.redis[:host] = ENV['REDIS_HOST']
+	config.redis[:port] = ENV['REDIS_PORT']
+	config.robot.adapter = :slack
+	config.adapters.slack.token = ENV['SLACK_TOKEN']
 end
