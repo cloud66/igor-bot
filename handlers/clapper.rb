68ddf0dffeb0c53e6bf4fc057e9aef0aa0350f0f
Vic van Gool <vic@cloud66.com>
2015-08-11 21:48:31 +0100

    bug

--- handlers/clapper.rb
+++ handlers/clapper.rb
@@ -1,7 +1,7 @@
 module Handlers
 	class Clapper < Lita::Handler
 
-		route(/^clap/i, :applause, command: true, help: { clapper: 'Gives you the clap' })
+		route(/^clap/i, :clap, command: true, help: { clapper: 'Gives you the clap' })
 
 		def clap(response)
 			response.reply "clap clap clap clap clap... ...yes, you've got the clap now"
