0dbe89f0f30102712f246b48022b1123f74dba46
Vic van Gool <vic@cloud66.com>
2015-08-11 21:44:26 +0100

    break the cache?

---
+++ handlers/clapper.rb
@@ -0,0 +1,12 @@
+module Handlers
+	class Clapper < Lita::Handler
+
+		route(/^clap/i, :applause, command: true, help: { clapper: 'Gives you the clap' })
+
+		def clap(response)
+			response.reply "clap clap clap clap clap... ...yes, you've got the clap now"
+		end
+	end
+
+	Lita.register_handler(Clapper)
+end
