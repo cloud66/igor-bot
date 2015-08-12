dafcf9026a4662f2ef365154d371e93713b93656
Vic van Gool <vic@cloud66.com>
2015-08-12 17:54:26 +0100

    some deploy enhancements

--- lita/handlers/deployer.rb
+++ lita/handlers/deployer.rb
@@ -4,18 +4,24 @@ module Lita
 	module Handlers
 		class Deployer < Lita::Handler
 
-			route(/^deploy\s+[a-zA-Z0-9-_]+\s*[a-zA-Z0-9-,_]*$/i, :deploy, command: true, help: { deployer: 'Deploys!' })
+			WARNING_SECONDS = 30
+			WARNING_MOD = 10
+			DEPLOY_PREFIX = 'igor_deployer'
+
+			route(/^deploy\s+[a-zA-Z0-9-_]+\s*[a-zA-Z0-9-,_]*$/i, :deploy, command: true, help: { deployer: 'Start deploys!' })
+			route(/^stop\sdeploy$/i, :stop_deploy, command: true, help: { deployer: 'Stop all pending deploys!' })
 
 			def deploy(context)
 				return unless context.message.command?
-				context.reply 'Yesssir!'
 
-				message = context.message.body
-				args = message.gsub(/^deploy\s+/, '')
-				parts = args.split(' ')
-				stack_name = parts[0]
-				if parts.size > 1
-					modifier = parts[1]
+				args = context.args
+				args = args.reject { |arg| arg =~ /^me$/ }
+
+				stack_name = args[0]
+				context.reply 'No no no... what is a me? hur hur!' and return if stack_name.nil? || stack_name.empty?
+
+				if args.size > 1
+					modifier = args[1]
 				else
 					modifier = ''
 				end
@@ -24,17 +30,60 @@ module Lita
 				context.reply "No no no... no stack found for \"#{stack_name}\"" and return if stack_envs.nil? || stack_envs.empty?
 				context.reply "No no no... more than one stack found for \"#{stack_name}\"" and return if stack_envs.size > 1
 
-				redeployment_hook_url = ENV.fetch(stack_envs.first)
-
+				stack_env = stack_envs.first
+				redeployment_hook_url = ENV.fetch(stack_env)
 				redeployment_hook_url = "#{redeployment_hook_url}?services=#{modifier}" unless modifier.empty?
+
+				deploy_key = "#{DEPLOY_PREFIX}.#{stack_env}"
+				exists = redis.get(deploy_key)
+				context.reply "No no no... deploy for \"#{stack_name}\" already in progress" and return if exists
+
+
+				context.reply "Yesssir! Deploying \"#{stack_name}\" in #{WARNING_SECONDS} seconds... (unless you tell me \"stop deploy\")"
+				start_time = Time.new
+				redis.setex(deploy_key, WARNING_SECONDS, 'true')
+
+				its_a_go = redis.get(deploy_key)
+				notified_at_zero = false
+
+				while its_a_go == 'true'
+					elapsed_seconds = (Time.new - start_time).round
+					remaining_seconds = (WARNING_SECONDS - elapsed_seconds)
+					remaining_seconds = 0 if remaining_seconds < 0
+
+					if remaining_seconds % WARNING_MOD == 0 && !notified_at_zero
+						context.reply "\"#{stack_name}\" deploying in #{WARNING_SECONDS - elapsed_seconds} seconds..."
+						notified_at_zero = true if remaining_seconds == 0
+					end
+
+					sleep(1)
+					its_a_go = redis.get(deploy_key)
+				end
+
+ 				if its_a_go == 'false'
+					redis.del(deploy_key)
+					context.reply "\"#{stack_name}\" deploy cancelled! Hur hur hur"
+					return
+				end
+
 				http_resp = HTTParty.post(redeployment_hook_url, {})
 
 				if http_resp.code != 200
-					context.reply 'No no no... got a non-200 response!'
+					context.reply "No no no... got a non-200 response from the \"#{stack_name}\" web hook!"
 				else
-					context.reply 'Whoop whoop its going!'
+					context.reply "Whoop whoop \"#{stack_name}\" deploy started! Hur hur hur"
 				end
 			end
+
+			def stop_deploy(context)
+				return unless context.message.command?
+				keys_wildcard = "*#{DEPLOY_PREFIX}.*"
+				context.reply 'Attempting to stop...'
+				redis.keys(keys_wildcard).each do |key|
+					redis.set(key, 'false')
+				end
+			end
+
 		end
 
 		Lita.register_handler(Deployer)
