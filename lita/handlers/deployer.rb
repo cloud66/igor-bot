a603ebb4236465c0bdcdc95af1e030683484a508
Vic van Gool <vic@cloud66.com>
2015-08-12 16:14:53 +0100

    deploy handler

--- lita/handlers/deployer.rb
+++ lita/handlers/deployer.rb
@@ -6,11 +6,11 @@ module Lita
 
 			route(/^deploy\s+[a-zA-Z0-9-_]+\s*[a-zA-Z0-9-,_]*$/i, :deploy, command: true, help: { deployer: 'Deploys!' })
 
-			def deploy(response)
-				return unless response.message.command?
-				response.reply 'Yesssir!'
+			def deploy(context)
+				return unless context.message.command?
+				context.reply 'Yesssir!'
 
-				message = response.message.body
+				message = context.message.body
 				args = message.gsub(/^deploy\s+/, '')
 				parts = args.split(' ')
 				stack_name = parts[0]
@@ -19,20 +19,20 @@ module Lita
 				else
 					modifier = ''
 				end
-				
-				stack_envs = ENV.keys.select { |stack_env| stack_env =~ /#{stack_name}/i }
-				response.reply "No no no... no stack found for \"#{stack_name}\"" and return if stack_envs.nil? || stack_envs.empty?
-				response.reply "No no no... more than one stack found for \"#{stack_name}\"" and return if stack_envs.size > 1
+
+				stack_envs = ENV.keys.select { |stack_env| stack_env =~ /^#{stack_name}/i }
+				context.reply "No no no... no stack found for \"#{stack_name}\"" and return if stack_envs.nil? || stack_envs.empty?
+				context.reply "No no no... more than one stack found for \"#{stack_name}\"" and return if stack_envs.size > 1
 
 				redeployment_hook_url = ENV.fetch(stack_envs.first)
 
 				redeployment_hook_url = "#{redeployment_hook_url}?services=#{modifier}" unless modifier.empty?
-				response = HTTParty.post(redeployment_hook_url, {})
+				http_resp = HTTParty.post(redeployment_hook_url, {})
 
-				if response.code != 200
-					response.reply 'No no no... got a non-200 response!'
+				if http_resp.code != 200
+					context.reply 'No no no... got a non-200 response!'
 				else
-					response.reply 'Whoop whoop its going!'
+					context.reply 'Whoop whoop its going!'
 				end
 			end
 		end
