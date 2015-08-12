eac38eb2e55ed4d229e28bfc68254400a244ef23
Vic van Gool <vic@cloud66.com>
2015-08-12 09:58:00 +0100

    where is the trigger word?

--- handlers/clapper.rb
+++ handlers/clapper.rb
@@ -4,7 +4,7 @@ module Handlers
 		route(/^clap/i, :clap, command: true, help: { clapper: 'Gives you the clap' })
 
 		def clap(response)
-			response.reply "clap clap clap clap clap... ...yes, you've got the clap now"
+			response.reply "clap clap clap clap clap... yes... you've got the clap now"
 		end
 	end
 
