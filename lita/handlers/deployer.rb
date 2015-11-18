bd940212a98f6d0dae51ac52482514261ef103c4
Vic van Gool <vic@cloud66.com>
2015-11-18 20:34:28 +0000

    igor messaging

--- lita/handlers/deployer.rb
+++ lita/handlers/deployer.rb
@@ -12,7 +12,7 @@ module Lita
 			DEPLOY_REGEX = /\A\s*((?<force>force)\s|)(re|)deploy(\sme\s|\s)(?<stack_name>[a-z-_]+)(::(?<service_name>[a-z-_,]+)|)(\s(now|immediately)|\s(?<later>asap|soon|later)|)\s*\z/i
 			STOP_REGEX = /\A(end|stop|cancel|quit|kill|terminate|end)\sdeploy(|ing|s|ment|ments)\z/i
 
-			route(DEPLOY_REGEX, :do_deploy, command: true, help: { deployer: "deploy: Deploy!\nMore info!" })
+			route(DEPLOY_REGEX, :do_deploy, command: true, help: { deployer: "deploy: Deploy!\nMore info here!" })
 			route(STOP_REGEX, :stop_deploy, command: true, help: { deployer: 'stop deploy: Stop all pending deploys!' })
 
 			def do_deploy(context)
@@ -38,21 +38,21 @@ module Lita
 					end
 				end
 				if flagged
-					context.reply '...Attempting to stop...'
+					context.reply 'OK: Sending stop signal...'
 				else
-					context.reply 'No no no... No pending deploys were found! Hur hur hur'
+					context.reply 'No: pending deploys not found'
 				end
 			end
 
 			private
 
 			def deploy(context, stack_name:, service_name:, force:, later:)
-				context.reply 'No no no... need to know what stack to deploy! Hur hur hur!' and return if stack_name.nil? || stack_name.empty?
-				context.reply 'No no no... force or wait? Can\'t do both! Hur hur hur!' and return if force && asap
+				context.reply 'No: Stack name is missing' and return if stack_name.nil? || stack_name.empty?
+				context.reply 'No: Use force OR wait, not both' and return if force && asap
 
 				stack_envs = ENV.keys.select { |stack_env| stack_env =~ /^#{stack_name}.*_hook/i }
-				context.reply "No no no... no stack found for \"#{stack_name}\"" and return if stack_envs.nil? || stack_envs.empty?
-				context.reply "No no no... more than one stack found for \"#{stack_name}\"" and return if stack_envs.size > 1
+				context.reply "No: \"#{stack_name} stack not found" and return if stack_envs.nil? || stack_envs.empty?
+				context.reply "No: \"#{stack_name} results in more than one stack match" and return if stack_envs.size > 1
 				stack_env = stack_envs.first
 				redeployment_hook_url = ENV.fetch(stack_env)
 
@@ -61,7 +61,7 @@ module Lita
 
 				deploy_key = "#{DEPLOY_PREFIX}.#{stack_env}"
 				exists = redis.get(deploy_key)
-				context.reply "No no no... an igor deploy for #{stack_name} is already waiting..." and return if exists == 'true'
+				context.reply "No: #{stack_name} already deploying" and return if exists == 'true'
 
 				# register this deploy
 				redis.set(deploy_key, 'true')
@@ -71,7 +71,7 @@ module Lita
 					deploy_status = get_deploy_status(redeployment_hook_url)
 					if worker_status[:is_busy] || deploy_status[:is_busy]
 						if later
-							context.reply "#{stack_name} is busy... I'll deploy it as soon as it's done! Hur hur hur!"
+							context.reply "OK: #{stack_name} deploy queued"
 							iterations = 0
 							while worker_status[:is_busy] || deploy_status[:is_busy]
 								iterations += 1
@@ -79,36 +79,36 @@ module Lita
 								worker_status = get_worker_status(stack_env)
 								deploy_status = get_deploy_status(redeployment_hook_url)
 
-								context.reply 'No no no... something is wrong! Waited more that 10 minutes...!' and return if iterations > 30
+								context.reply 'No: Timed out after 10 minutes!' and return if iterations > 30
 
 								its_a_go = redis.get(deploy_key)
 								if its_a_go == 'false'
 									redis.del(deploy_key)
-									context.reply "#{stack_name} deploy cancelled! Hur hur hur"
+									context.reply "OK: #{stack_name} deploy cancelled"
 									return
 								end
 							end
 						else
-							context.reply "No no no... #{stack_name} is busy on prod... not deploying!" and return if deploy_status[:is_busy]
-							context.reply "No no no... #{stack_name} has busy workers... not deploying!" and return if worker_status[:is_busy]
+							context.reply "No: #{stack_name} is busy" and return if deploy_status[:is_busy]
+							context.reply "No: #{stack_name} workers are busy" and return if worker_status[:is_busy]
 						end
 					end
 
-					context.reply "Hmmmmm... #{stack_name} - #{worker_status[:error]}" unless worker_status[:error].nil?
-					context.reply "Hmmmmm... #{stack_name} - #{deploy_status[:error]}" unless deploy_status[:error].nil?
+					context.reply "Warning: #{stack_name} - #{worker_status[:error]}" unless worker_status[:error].nil?
+					context.reply "Warning: #{stack_name} - #{deploy_status[:error]}" unless deploy_status[:error].nil?
 				end
 
 				redis.set(deploy_key, 'false')
 				http_resp = HTTParty.post(redeployment_hook_url, {}) rescue nil
 				if http_resp.nil?
-					context.reply "No no no... got an unhandled exception response from the #{stack_name} web hook!"
+					context.reply "No: #{stack_name} web hook unhandled exception"
 				elsif http_resp.code != 200
-					context.reply "No no no... got a non-200 response from the #{stack_name} web hook!"
+					context.reply "No: #{stack_name} web hook non-200 response"
 				else
 					if service_name.nil?
-						context.reply "Whoop whoop! #{stack_name} deploy started! Hur hur hur"
+						context.reply "OK: #{stack_name} deploy started..."
 					else
-						context.reply "Whoop whoop! #{stack_name} (#{service_name}) deploy started! Hur hur hur"
+						context.reply "OK: #{stack_name} [#{service_name}] deploy started..."
 					end
 
 					# wait for deploy to start
@@ -121,9 +121,9 @@ module Lita
 						iterations += 1
 						sleep(20)
 						deploy_status = get_deploy_status(redeployment_hook_url)
-						context.reply 'No no no... something is wrong! Waited more that 10 minutes...!' and return if iterations > 20
+						context.reply 'No: Timed out after 10 minutes!' and return if iterations > 30
 					end
-					context.reply "Wooohooo #{stack_name} finished deploying!"
+					context.reply "OK: #{stack_name} deploy complete!"
 				end
 			ensure
 				# always get rid of that redis key when done
@@ -137,7 +137,7 @@ module Lita
 
 				http_resp = HTTParty.get(check_url) rescue nil
 				if http_resp.nil? || http_resp.code != 200
-					return { is_busy: false, error: 'Could not get the deploy state... continuing!' }
+					return { is_busy: false, error: 'Busy state unavailable... continuing' }
 				else
 					params = http_resp.parsed_response
 					is_busy = params['response']['is_busy']
@@ -149,7 +149,7 @@ module Lita
 				return { is_busy: false } if stack_env != 'STAGING_HOOK'
 				http_resp = HTTParty.get(STAGING_WORKERS_URL) rescue nil
 				if http_resp.nil? || http_resp.code != 200
-					return { is_busy: false, error: 'Could not get the worker state... continuing!' }
+					return { is_busy: false, error: 'Worker state unavailable... continuing' }
 				else
 					params = http_resp.parsed_response
 					worker_count = params['response']['workers_info'].size rescue 0
