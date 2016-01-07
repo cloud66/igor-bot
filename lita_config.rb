316f7be5cd9f0dd0cc320bdc37a55de9135df528
Vic van Gool <vic@cloud66.com>
2016-01-07 20:59:25 +0000

    quick fix attempt

--- lita_config.rb
+++ lita_config.rb
@@ -2,6 +2,7 @@ APP_ROOT_PATH = File.expand_path(File.dirname(__FILE__))
 
 Dir['lita/lib/*.rb'].each { |file| require_relative file }
 Dir['lita/models/*.rb'].each { |file| require_relative file }
+Dir['lita/handlers/abstract_*.rb'].each { |file| require_relative file }
 Dir['lita/handlers/*.rb'].each { |file| require_relative file }
 Lita.configure do |config|
 	config.robot.log_level = (ENV.fetch('LITA_INFO_LEVEL') { :warn }).to_sym
