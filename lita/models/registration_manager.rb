92916a28af8e65a254745963d26e91e63737a30b
Pierre Martinot <pierre@cloud66.com>
2016-12-06 15:54:12 +0000

    igor se token each time

--- lita/models/registration_manager.rb
+++ lita/models/registration_manager.rb
@@ -8,13 +8,11 @@ module Models
 		include ::Singleton
 		attr_accessor :access_token
 
-		#TOKEN_LOCATION = "#{APP_ROOT_PATH}/config/.slack-bot-auth.json"
 		TOKEN_LOCATION = "/opt/chat-ops-common/c66-token.json"
 		APP_UID = "b5de172cffa26c681954c96adb55fd8d5d7c5298bcc7669e4969241fa92b413f"
 		APP_SECRET = "1d639bc0b2296aebdb7f0737645545ea2506ca8d20d0f9e34d976ba704debf23"
 
 		def initialize
-			puts('?')
 			load_c66_token_info(TOKEN_LOCATION)
 		end
 
@@ -54,11 +52,10 @@ module Models
 				local_token = config[:local_token]
 				client = OAuth2::Client.new(APP_UID, APP_SECRET, :site => 'https://stage.cloud66.com')
 				self.access_token = OAuth2::AccessToken.new(client, config[:local_token])
-				if File.exist?("/opt/chat-ops-common/is-token")
-				else
-					puts('eho')
+				#if File.exist?("/opt/chat-ops-common/is-token")
+				#else
 					set_token_info(local_token);
-				end
+				#end
 			end
 		rescue => exc
 			return exc.message
