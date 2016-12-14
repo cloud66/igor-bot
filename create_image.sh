66ef87ea875319260bc43646422b61d5a66a9c4f
Pierre Martinot <pierre@cloud66.com>
2016-12-14 19:16:00 +0000

    final doc + github page + docker compose

--- create_image.sh
+++ create_image.sh
@@ -1,12 +1,12 @@
 #/bin/bash
-if [ $1 ]; then
- git pull
- docker build -t cloud66/igor:$1 .
- docker build -t cloud66/igor-webpage:$1 -f registration_webpage/Dockerfile .
- docker login
- docker push cloud66/igor:$1
- docker push cloud66/igor-webpage:$1
+if [ $1 ]
+then
+	git checkout master
+ 	git pull origin master
+ 	docker build -t cloud66/igor:latest -t cloud66/igor:$1 .
+ 	docker build -t cloud66/igor-webpage:latest -t cloud66/igor-webpage:$1 -f registration_webpage/Dockerfile .
+ 	docker push cloud66/igor:$1
+ 	docker push cloud66/igor-webpage:$1
 else
- echo 'The version of the new build is required'
+	echo 'Please pass the new version of the build as an argument!'
 fi
-#docker build -f registration_webpage/Dockerfile . -t fuck/sticks:0.10
